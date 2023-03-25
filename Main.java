f9d2b9a4c3e9d01ad61cccc61960c0c3a521fa79
AaronDLopez04 <126019490+AaronDLopez04@users.noreply.github.com>
2023-03-24 21:43:09 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,16 @@ public class Main {
     public static void main(String[] args) {
         MaquinaDeCafe machine = new MaquinaDeCafe();
         System.out.println(machine);
-        machine.servirCafe("Americano");
-        machine.servirCafe("Expreso");
-        machine.servirCafe("Capuchino");
+        machine.mostrarRecursos();
+        for (int i = 0; i < 25; i++) {
+            machine.servirCafe("Americano");
+        }
+        for (int i = 0; i < 12; i++) {
+            machine.servirCafe("Expreso");
+        }
+        for (int i = 0; i < 14; i++) {
+            machine.servirCafe("Capuchino");
+        }
         machine.mostrarRecursos();
         System.out.println(machine);
     }
